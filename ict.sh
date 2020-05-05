0e07b2ecde34248ae727eab210eddb65bcebf047
Neel Smith <dnsmith.neel@gmail.com>
2020-05-05 09:05:16 -0400

    Tidy comments

--- ict.sh
+++ ict.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 #
-# Start
+# Start a docker container with citable image service,
+# optionally setting values for port on the host OS,
+# and directories to mount in the container.
+#
+# Usage:
+#
+#    ict.sh [-p|--port PORT_NUMBER] [/PATH/TO/IMAGE/ROOT/] [/PATH/TO/NAMESPACE1 /PATH/TO/NAMESPACE2... /PATH/TO/NAMESPACEN]
+#
 
+# Default values for two settings we will optionally
+# modify from command-line arguments:
 PORT=8080
 MOUNTS=
 
+# Convenience function to glue bash array together
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
 for arg in "$@"
 do
   case $arg in
     -p|--port)
+    # Override default PORT setting:
     PORT=$2
-    #echo PORT setting $PORT
     shift
     shift
     ;;
 
     *)
-    #echo ARG: "# $1 #"
     if echo "$1" | grep -q '/$'; then
-      #echo "Trailing slash in " $1
+      #Trailing slash in: mount as root of service:
       MOUNTS+=" -v $1:/pyramids/"
       echo $MOUNTS
     elif [[ $1 == " " ]]; then
       shift
     else
-      #echo "No trailing slash: explode #" $1 "#"
+      # No trailing slash: mount as namespace
       oldIFS=$IFS
       IFS=/
       read -ra PATHPARTS <<<"$1"
@@ -47,4 +56,5 @@ do
   esac
 done
 
+# Run container with settings from command-line arguments:
 docker run -p ${PORT}:80 --rm -it -v $(pwd):/work ${MOUNTS} neelsmith/ict:latest  /bin/bash
