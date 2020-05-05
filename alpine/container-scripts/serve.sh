f734059e789ab1651f9e744cbf7745b804e55a2e
Neel Smith <dnsmith.neel@gmail.com>
2020-05-05 14:23:50 -0400

    White space

--- alpine/container-scripts/serve.sh
+++ alpine/container-scripts/serve.sh
@@ -7,7 +7,6 @@ if test -f "$SETTING_FILE"; then
     echo "Configuring custom port setting to " $PORT
     /bin/sed -i "s#http://127.0.0.1:8080/#http://127.0.0.1:${PORT}/#" /www/ict2/js/ict2.js
     /bin/sed -i "s#http://127.0.0.1:8080/#http://127.0.0.1:${PORT}/#g" /www/index.html
-
 fi
 
 /usr/bin/spawn-fcgi -f /fcgi-bin/iipsrv.fcgi -a 127.0.0.1 -p 9000
